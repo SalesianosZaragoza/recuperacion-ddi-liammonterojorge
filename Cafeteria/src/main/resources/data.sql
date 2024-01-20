79f23b2dcdddfd8086e007ada03ecaa8821c1b01
LiamRamiro <ramiro.emlia22@zaragoza.salesianos.edu>
2024-01-20 12:25:59 +0100

    restructuracion proyecto

--- Cafeteria/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Producto (nombre, precio) VALUES ('Café', 2);
-INSERT INTO Producto (nombre, precio) VALUES ('Café con leche', 3);
-INSERT INTO Producto (nombre, precio) VALUES ('Cookies', 1);
-INSERT INTO Producto (nombre, precio) VALUES ('Té', 2);
-
-
