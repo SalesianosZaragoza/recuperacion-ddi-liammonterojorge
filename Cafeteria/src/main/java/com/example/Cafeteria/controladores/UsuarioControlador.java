549e06bc9f8b362953a709047a6a0b505b126141
LiamRamiro <ramiro.emlia22@zaragoza.salesianos.edu>
2024-02-13 12:29:32 +0100

    hola

--- Cafeteria/src/main/java/com/example/Cafeteria/controladores/UsuarioControlador.java
+++ Cafeteria/src/main/java/com/example/Cafeteria/controladores/UsuarioControlador.java
@@ -1,9 +1,14 @@
 package com.example.Cafeteria.controladores;
 
+import com.example.Cafeteria.modelos.Producto;
+import com.example.Cafeteria.modelos.Usuario;
 import com.example.Cafeteria.repositorio.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class UsuarioControlador {
@@ -12,8 +17,11 @@ public class UsuarioControlador {
     UsuarioRepositorio usuarioRepositorio;
 
     @RequestMapping("/listarUsuario")
-    public String listaUsuario(){
+    public String listaUsuario(Model model){
+        List<Usuario> listaUsuario = UsuarioRepositorio.getTodosUsuario();
+        model.addAttribute("listaUsuario", listaUsuario);
         return "listaUsuario";
+
     }
 
     @RequestMapping("/formInsertarUsuario")
--- Cafeteria/src/main/java/com/example/Cafeteria/repositorio/UsuarioRepositorio.java
+++ Cafeteria/src/main/java/com/example/Cafeteria/repositorio/UsuarioRepositorio.java
@@ -1,13 +1,33 @@
 package com.example.Cafeteria.repositorio;
 
+import com.example.Cafeteria.modelos.Producto;
+import com.example.Cafeteria.modelos.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 @Repository
 public class UsuarioRepositorio {
 
     @Autowired
     UsuarioRepositorio usuarioRepositorio;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Usuario> getTodosUsuario(){
+        String query = "SELECT * FROM usuario;";
+        List<Usuario> listaUsuario = jdbcTemplate.query(query, new UsuarioRowMapper());
+        return listaUsuario;
+    }
+
+
+
+
+
+
 
 
 }
